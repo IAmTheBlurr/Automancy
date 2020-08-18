53483e192ebb0e64d669502c0de39c2de3767ac3
Jonathan Craig <blurr@iamtheblurr.com>
2020-08-17 23:00:21 -0700

    setup.py -> Updates values

--- setup.py
+++ setup.py
@@ -6,17 +6,23 @@ setuptools.setup(
     author='Jonathan Craig',
     author_email='blurr@iamtheblurr.com',
     classifiers=[
-        'Programming Language :: Python :: 3.7'
-        'Topic :: Web Automation :: Quality Assurance'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Internet :: WWW/HTTP :: Browsers'
     ],
     packages=setuptools.find_packages(),
     url='https://github.com/iamtheblurr/automancy',
     license='MIT',
     install_requires=[
         'chronomancy',
-        'lxml==4.2.5',
-        'selenium==3.14.1',
-        'webvtt-py==0.4.2',
+        'lxml',
+        'pytest',
+        'selenium',
+        'webvtt-py',
     ],
     long_description=open('README.md', encoding='utf8').read(),
 )
