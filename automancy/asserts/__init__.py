0cb7449aff478cf25e52b7d4b5555ec0a25e3012
Jonathan Craig <blurr@iamtheblurr.com>
2020-08-18 16:10:20 -0700

    Asserts -> Adds __init__.py imports

--- automancy/asserts/__init__.py
+++ automancy/asserts/__init__.py
@@ -1,1 +1,4 @@
+from .gains_clickability import GainsClickability
+from .gains_existence import GainsExistence
+from .gains_visibility import GainsVisibility
 from .video_begins_playing import VideoBeginsPlaying
