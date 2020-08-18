f22215f8c793cd4cdb19db9d43f70d22f636c8ec
Jonathan Craig <blurr@iamtheblurr.com>
2020-08-18 15:10:10 -0700

    AssertCore -> Articulates more core functionality

--- automancy/asserts/assert_core.py
+++ automancy/asserts/assert_core.py
@@ -1,9 +1,20 @@
 """ ./asserts/assert_core.py """
 import unittest
 
+from time import sleep
+
+from ..core import Elemental
+
 
 class AssertCore(unittest.TestCase()):
-    def __init__(self, sleep_time, timeout_count, max_timeouts):
-        self.sleep_time = sleep_time
-        self.timeout_count = timeout_count
+    def __init__(self, element, sleep_time: float = 0.25, max_timeouts: int = 10):
+        self.element = element
         self.max_timeouts = max_timeouts
+        self.sleep_time = sleep_time
+        self.timeout_count = 0
+        self.sleep = sleep
+
+    @staticmethod
+    def verify_is_elemental(element):
+        if not issubclass(element, Elemental):
+            raise TypeError('Input element must be a subclass of Elemental, found: {}'.format(type(element)))
