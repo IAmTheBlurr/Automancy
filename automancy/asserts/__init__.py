1ffd007f5528bbe52a3ad31ac58a7346077c4dfd
Jonathan Craig <blurr@iamtheblurr.com>
2020-08-20 17:27:39 -0700

    Asserts -> Deprecate all files and directories
    Now that we're using the new TacticalAsserts design officially.

--- automancy/asserts/__init__.py
+++
@@ -1,4 +0,0 @@
-from .gains_clickability import GainsClickability
-from .gains_existence import GainsExistence
-from .gains_visibility import GainsVisibility
-from .video_begins_playing import VideoBeginsPlaying
