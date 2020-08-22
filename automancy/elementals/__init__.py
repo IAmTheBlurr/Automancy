3a02fe29b706e736b22ead1ec6c242e698f9983c
Jonathan Craig <blurr@iamtheblurr.com>
2020-08-22 14:01:41 -0700

    Organisms -> Adds the Table elemental and it's sub-modules

--- automancy/elementals/__init__.py
+++ automancy/elementals/__init__.py
@@ -1,4 +1,4 @@
 from .atoms.button import Button
 from .atoms.label import Label
 from .atoms.text_input import TextInput
-from .organisms import Grid
+from .organisms import Grid, Table, TableOptions
--- automancy/elementals/organisms/__init__.py
+++ automancy/elementals/organisms/__init__.py
@@ -1,1 +1,3 @@
 from .grid.grid import Grid
+from .table.table import Table
+from .table.table_options import TableOptions
