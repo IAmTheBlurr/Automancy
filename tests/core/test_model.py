6957ff33718a481413bd2d161738cd8423b52fc5
Jonathan Craig <blurr@iamtheblurr.com>
2020-10-05 17:59:02 -0700

    Tests -> Adds test_model.py with failing tests

---
+++ tests/core/test_model.py
@@ -0,0 +1,29 @@
+import pytest
+
+from automancy import Modal
+
+
+class TestModel(object):
+    def test_system_name_creation(self):
+        pytest.fail()
+
+    def test_exception_with_space_in_system_name(self):
+        pytest.fail()
+
+    def test_overwrite_existing_model_component(self):
+        pytest.fail()
+
+    def test_cannot_overwrite_existing_model_component_without_overwrite_being_true(self):
+        pytest.fail()
+
+    def test_added_component_is_concatenated_parent_plus_input(self):
+        pytest.fail()
+
+    def test_added_component_is_set_as_a_property_of_the_parent(self):
+        pytest.fail()
+
+    def test_added_component_exists_in_parent_children_property(self):
+        pytest.fail()
+
+    def test_added_components_properties_are_correct(self):
+        pytest.fail()
