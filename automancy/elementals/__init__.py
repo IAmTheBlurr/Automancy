ff26dc30a0090667c66a318e94bd9b8890245e92
Jonathan Craig <blurr@iamtheblurr.com>
2020-10-07 19:36:32 -0700

    Atoms -> Adds the Image module and imports
    Import changes bubble up through the root level __init__.py

--- automancy/elementals/__init__.py
+++ automancy/elementals/__init__.py
@@ -1,3 +1,3 @@
-from .atoms import Button, Checkbox, FileInput, Label, TextInput
+from .atoms import Button, Checkbox, FileInput, Image, Label, TextInput
 from .molecules import Modal
 from .organisms import Grid, Table, TableOptions
--- automancy/elementals/atoms/__init__.py
+++ automancy/elementals/atoms/__init__.py
@@ -1,5 +1,6 @@
 from .button import Button
 from .checkbox import Checkbox
 from .file_input import FileInput
+from .image import Image
 from .label import Label
 from .text_input import TextInput
