59f5aa093ed949f4806e56fac2e4be6be6cff2f5
Jonathan Craig <blurr@iamtheblurr.com>
2020-10-09 21:54:58 -0700

    Atoms -> Updates docstrings for Button

--- automancy/elementals/atoms/button.py
+++ automancy/elementals/atoms/button.py
@@ -3,10 +3,13 @@ from automancy.core import Elemental
 
 
 class Button(Elemental):
-    """ Object model for a button web element """
+    """ Atom Elemental for button DOM objects """
     def __init__(self, locator: str, human_name: str, system_name: str):
         """
         Represents a basic button DOM element of any kind which could exist on a page.
+
+        It's not a sin to use this class for anything which you might think as a button
+        but isn't necessarily a <button> element.
 
         Args:
             locator (str): xpath string for the lookup
