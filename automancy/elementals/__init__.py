32abccc18b63b1364d2ffcec78b3a90112429661
Jonathan Craig <blurr@iamtheblurr.com>
2020-10-14 17:03:12 -0700

    Organisms -> Implements Dropdown and DropdownOptions classes

--- automancy/elementals/__init__.py
+++ automancy/elementals/__init__.py
@@ -1,3 +1,3 @@
 from .atoms import Button, Checkbox, FileInput, Image, Label, Link, Radio, SVG, SVGPath, Switch, TextBox, TextInput
 from .molecules import Carousel, Form, Modal, Slider
-from .organisms import Clock, ClockOptions, Grid, Table, TableOptions
+from .organisms import Clock, ClockOptions, Dropdown, DropdownOptions, Grid, Table, TableOptions
--- automancy/elementals/organisms/__init__.py
+++ automancy/elementals/organisms/__init__.py
@@ -1,3 +1,4 @@
 from .clock import Clock, ClockOptions
+from .dropdown import Dropdown, DropdownOptions
 from .grid import Grid
 from .table import Table, TableOptions
