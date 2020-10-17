260c5c415ba643794598e8f06075f8af4ffa04c4
Jonathan Craig <blurr@iamtheblurr.com>
2020-10-16 17:42:12 -0700

    Organisms -> Implements HTML5VideoPlayer classes

--- automancy/__init__.py
+++ automancy/__init__.py
@@ -2,4 +2,4 @@ from .core.tactical_asserts import TacticalAsserts
 from .ecosystems import Page
 from .elementals.atoms import Button, Checkbox, FileInput, Image, Label, Link, Radio, SVG, SVGPath, Switch, TextBox, TextInput
 from .elementals.molecules import Carousel, DialogBox, Form, Modal, Slider
-from .elementals.organisms import Clock, ClockOptions, Dropdown, DropdownOptions, Grid, Table, TableOptions
+from .elementals.organisms import Clock, ClockOptions, Dropdown, DropdownOptions, Grid, HTML5VideoPlayer, OptionTree, OptionTreeBranch, OptionTreeBranches, Table, TableOptions
